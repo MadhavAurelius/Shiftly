0576bab67db600d29a3d6158e48253e73fd54ce3
MadhavAurelius <madhu.polyglot@gmail.com>
2025-12-23 22:45:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import "./App.css";
+import location from "./assets/Location.png";
+import bgVideo from "./assets/3d.mp4";
+import photo from "./assets/phone.jpg.png";
+import video from "./assets/video.mp4"
+function App() {
+  return (
+    <>
+      <div className="container" style={{ color: "#160470ff" }}>
+        <div>
+          <h1 style={{ color: "#f50303ff", textAlign: "center", marginLeft: 450, fontSize:80}}>
+            Shiftly
+          </h1>
+
+          <h2 style={{ color: "#163913ff", textAlign: "center", marginLeft:450, fontSize:30}}>
+            Furniture moving within and outside Hail
+            <a
+              href="https://maps.app.goo.gl/bCmCHAPZnGmvfanb9"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="location" src={location} alt="Location" />
+            </a>
+
+            <br /><br />
+            Buying <br /><br />
+            Selling <br /><br />
+            Moving <br /><br />
+            Dismantling <br /><br />
+            Assembling Furniture <br /><br />
+            Within and Outside Hail <br /><br />
+            Including Dismantling and Assembling <br /><br />
+            +966 59 277 0758
+          </h2>
+
+          <h2 dir="rtl" style={{ textAlign: "right", marginRight: 100 }}>
+            نقل العفش داخل حايل وخارج حايل<br />
+            بيع - شراء - نقل - الأثاث وفك وتركيب<br />
+            داخل حايل وخارج حايل مع فك وتركيب<br /><br />
+            <span dir="ltr">+966 59 277 0758</span>
+          </h2>
+
+          {/* WhatsApp */}
+          <a
+            href="https://wa.me/966592770758?text=Hi%20I%20am%20contacting%20from%20your%20website"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsapp-float"
+          >
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/023/986/550/non_2x/whatsapp-logo-whatsapp-logo-transparent-whatsapp-icon-transparent-free-free-png.png"
+              alt="WhatsApp"
+            />
+          </a>
+
+          {/* Call */}
+          <a href="tel:+966592770758" className="call-float">
+            <img className="call" src={photo} alt="Call" />
+          </a>
+
+          {/* Background video */}
+          <video id="bg-video" autoPlay muted loop playsInline>
+            <source src={video} type="video/mp4" />
+            Your browser does not support HTML5 video.
+          </video>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
